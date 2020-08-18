655bc3dc584d092e784975283bb8b6658e79724f
nonghao <470453872@qq.com>
2020-08-18 15:39:21 +0800

    res

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('./routers/users');
 const debug = require('debug')('my-application');
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use("/api/users",users);
 
 app.listen(3030, (req, res) => {
--- server/routers/users.js
+++ server/routers/users.js
@@ -1,11 +1,46 @@
 const express = require("express");
+const isEmpty = require("lodash/isEmpty");
+const validator = require("validator");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send({
-        message: "Hello!"
-    });
+const validatorInput = (data) => {
+    if(isEmpty(data)){
+        data = {};
+    }
+
+    var errors = {};
+    if(!data.phone || validator.isEmpty(data.phone)){
+        errors.phone = "请输入有效的手机号";
+    }
+    if(!validator.isEmail(data.email)){
+        errors.email = "请输入邮箱";
+    }
+    if(validator.isEmpty(data.password)){
+        errors.password = "请输入密码";
+    }
+
+    if(!validator.equals(data.password, data.passwordConfirmation)){
+        errors.passwordConfirmation = "两次输入的密码不相同";
+    }
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+}
+
+router.post("/", (req, res) => {
+    console.log("req data==", req.body);
+    const result = validatorInput(req.body);
+    console.log("result",result);
+    if(result.isValid){
+        res.status(200).json({errorCode: 0});
+    }else{
+        res.status(400).json({
+            errorCode: 400,
+            msg: result.errors
+        });
+    }
 });
 
 module.exports = router;
