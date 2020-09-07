9b42946e624f49dd7c55d9ecac15301a8df06509
Nong Hao <470453872@qq.com>
2020-09-08 00:04:33 +0800

    登录接口

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const users = require('./routers/users');
+const auth = require("./routers/auth");
 const debug = require('debug')('my-application');
 const bodyParser = require("body-parser");
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/api/users",users);
+app.use("/api/auth",auth);
 
 app.listen(3030, (req, res) => {
     debug("服务端运行在3030端口");
---
+++ server/routers/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const sqlFn = require("../mysql");
+
+const router = express.Router();
+
+router.post("/", function(req, res){
+    var sql = "select * from users where phone = ? && password = ?";
+    var arr = [req.body.phone, req.body.password];
+    sqlFn(sql, arr, function(res){
+        if(res){
+            res.send({errorCode: 0, msg: "登录成功"});
+        }else{
+            res.send({errorCode: -1, msg: "用户名或密码错误"});
+        }
+    });
+})
+
+module.exports = router;
